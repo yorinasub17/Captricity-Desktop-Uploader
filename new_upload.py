88650909bdc4d213738488ca03dbe88f227c4233
Yoriyasu Yano <yoriy@captricity.com>
2014-04-11 17:37:02 -0700

    Use batches to reflect Captricity2.0 update

--- new_upload.py
+++ new_upload.py
@@ -13,7 +13,9 @@ from utils import delete_layout, natural_sort
 class NewUploadWindow(QtGui.QWidget):
     MENU_WINDOW = 0
     LIST_WINDOW = 1
-    CONFIRM_WINDOW = 2
+    CREATE_WINDOW = 2
+    CONFIRM_WINDOW = 3
+    DEFAULT_BATCH_NAME = 'New Batch'
 
     def __init__(self, cap_client, main_window, parent=None):
         self.cap_client = cap_client
@@ -23,15 +25,23 @@ class NewUploadWindow(QtGui.QWidget):
         self.menu_layout = QtGui.QVBoxLayout()
         self.menu_widget = QtGui.QWidget()
         self.menu_widget.setLayout(self.menu_layout)
+
         self.list_layout = QtGui.QVBoxLayout()
         self.list_widget = QtGui.QWidget()
         self.list_widget.setLayout(self.list_layout)
+
         self.confirm_layout = QtGui.QVBoxLayout()
         self.confirm_widget = QtGui.QWidget()
         self.confirm_widget.setLayout(self.confirm_layout)
+
+        self.create_layout = QtGui.QVBoxLayout()
+        self.create_widget = QtGui.QWidget()
+        self.create_widget.setLayout(self.create_layout)
+
         self.layout_ = QtGui.QStackedLayout()
         self.layout_.addWidget(self.menu_widget)
         self.layout_.addWidget(self.list_widget)
+        self.layout_.addWidget(self.create_widget)
         self.layout_.addWidget(self.confirm_widget)
 
         super(NewUploadWindow, self).__init__(parent)
@@ -39,31 +49,25 @@ class NewUploadWindow(QtGui.QWidget):
         self.setLayout(self.layout_)
         self.load_menu_window()
 
-    def upload_to_document(self):
-        document_getter = lambda: self.cap_client.read_documents('?user_visible=true&active=true')
-        self.load_list_window(document_getter, self.document_upload_callback_func_generator)
+        self.new_batch_name = NewUploadWindow.DEFAULT_BATCH_NAME
 
-    def upload_to_job(self):
-        job_getter = lambda: self.cap_client.read_jobs('?status=setup')
-        self.load_list_window(job_getter, self.job_upload_callback_func_generator)
+    def upload_to_batch(self):
+        batch_getter = lambda: self.cap_client.read_batches('?status=setup')
+        self.load_list_window(batch_getter, self.batch_upload_callback_func_generator)
+
+    def create_batch(self):
+        new_batch = self.cap_client.create_batches({'name': self.new_batch_name})
+        self.batch_upload_callback_func_generator(new_batch)()
 
     def cancel(self):
         self.close()
         self.main_window.close_new_upload()
 
-    def document_upload_callback_func_generator(self, document):
+    def batch_upload_callback_func_generator(self, batch):
         def callback():
             directory = str(QtGui.QFileDialog.getExistingDirectory(None, 'Select directory to upload'))
             if directory:
-                self.upload_manager = UploadToDocument(self.main_window.cap_client, self.main_window.pool, document['id'], document['sheet_count'], directory)
-                self.load_confirm_window(self.upload_manager)
-        return callback
-
-    def job_upload_callback_func_generator(self, job):
-        def callback():
-            directory = str(QtGui.QFileDialog.getExistingDirectory(None, 'Select directory to upload'))
-            if directory:
-                self.upload_manager = UploadToJob(self.main_window.cap_client, self.main_window.pool, job['id'], job['sheet_count'], directory)
+                self.upload_manager = UploadToBatch(self.main_window.cap_client, self.main_window.pool, batch['id'], directory)
                 self.load_confirm_window(self.upload_manager)
         return callback
 
@@ -82,17 +86,13 @@ class NewUploadWindow(QtGui.QWidget):
         self.close()
         self.resize(350, 250)
 
-        upload_to_document = QtGui.QPushButton('Upload to Template', self)
-        self.connect(upload_to_document, QtCore.SIGNAL('clicked()'), self.upload_to_document)
-
-        upload_to_job = QtGui.QPushButton('Upload to Job', self)
-        self.connect(upload_to_job, QtCore.SIGNAL('clicked()'), self.upload_to_job)
+        upload_to_batch = QtGui.QPushButton('Upload to Batch', self)
+        self.connect(upload_to_batch, QtCore.SIGNAL('clicked()'), self.upload_to_batch)
 
         cancel = QtGui.QPushButton('Cancel', self)
         self.connect(cancel, QtCore.SIGNAL('clicked()'), self.load_prev_window)
 
-        self.menu_layout.addWidget(upload_to_document)
-        self.menu_layout.addWidget(upload_to_job)
+        self.menu_layout.addWidget(upload_to_batch)
         self.menu_layout.addWidget(cancel)
 
     def load_list_window(self, get_captricity_objects, callback_func_generator):
@@ -110,6 +110,9 @@ class NewUploadWindow(QtGui.QWidget):
             self.connect(button, QtCore.SIGNAL('clicked()'), callback_func_generator(obj))
             buttons.append(button)
 
+        create_button = QtGui.QPushButton('Create', self)
+        self.connect(create_button, QtCore.SIGNAL('clicked()'), self.load_create_window)
+
         refresh_button = QtGui.QPushButton('Refresh', self)
         self.connect(refresh_button, QtCore.SIGNAL('clicked()'), lambda: self.load_list_window(get_captricity_objects, callback_func_generator))
 
@@ -118,6 +121,7 @@ class NewUploadWindow(QtGui.QWidget):
 
         actions_layout = QtGui.QHBoxLayout()
         actions_layout.addStretch(1)
+        actions_layout.addWidget(create_button)
         actions_layout.addWidget(refresh_button)
         actions_layout.addWidget(back_button)
 
@@ -156,6 +160,36 @@ class NewUploadWindow(QtGui.QWidget):
         self.confirm_layout.addWidget(list_widget)
         self.confirm_layout.addLayout(actions_layout)
 
+    def load_create_window(self):
+        delete_layout(self.create_layout, delete_container=False)
+        self.layout_.setCurrentIndex(NewUploadWindow.CREATE_WINDOW)
+
+        self.load_prev_window = self.load_current_window
+        self.load_current_window = lambda: self.load_create_window()
+
+        instructions = QtGui.QLabel('Name this batch')
+
+        self.new_batch_name = NewUploadWindow.DEFAULT_BATCH_NAME
+        name_box = QtGui.QLineEdit(self)
+        def name_box_changed(text):
+            self.new_batch_name = text
+        name_box.textChanged[str].connect(name_box_changed)
+
+        next_button = QtGui.QPushButton('Next')
+        self.connect(next_button, QtCore.SIGNAL('clicked()'), self.create_batch)
+
+        cancel_button = QtGui.QPushButton('Cancel', self)
+        self.connect(cancel_button, QtCore.SIGNAL('clicked()'), self.load_prev_window)
+
+        actions_layout = QtGui.QHBoxLayout()
+        actions_layout.addStretch(1)
+        actions_layout.addWidget(cancel_button)
+        actions_layout.addWidget(next_button)
+
+        self.create_layout.addWidget(instructions)
+        self.create_layout.addWidget(name_box)
+        self.create_layout.addLayout(actions_layout)
+
 class UploadTracker(QtGui.QWidget):
     image_uploaded = QtCore.Signal()
 
@@ -179,26 +213,25 @@ class UploadTracker(QtGui.QWidget):
         self.pbar.setValue(self.upload_manager.progress)
 
 class Uploader(object):
-    def __init__(self, client, pool, sheet_count, directory):
+    def __init__(self, client, pool, directory):
         self.result_set = None
         self.linked_pbar = None
         self.client = client
         self.pool = pool
         self.directory = directory
         self.files = natural_sort(glob.glob(os.path.join(directory, '*')))
-        self.grouped_files = list(itertools.izip_longest(*(iter(self.files),) * sheet_count))
 
     def link_pbar(self, pbar):
         self.linked_pbar = pbar
 
     def start(self):
         assert self.linked_pbar is not None
-        if self.job_id is None:
-            new_job = self.client.create_jobs({'document_id': str(self.document_id)})
-            self.job_id = new_job['id']
-        self.result_set = [0 for i in range(len(self.grouped_files))]
-        for i, files in enumerate(self.grouped_files):
-            self.pool.apply_async(upload_iset, (self.client.api_token, self.job_id, files), callback=self.upload_finished_callback_generator(i))
+        if self.batch_id is None:
+            new_batch = self.client.create_batchs()
+            self.batch_id = new_batch['id']
+        self.result_set = [0 for i in xrange(len(self.files))]
+        for idx, f in enumerate(self.files):
+            self.pool.apply_async(upload_file, (self.client.api_token, self.batch_id, f), callback=self.upload_finished_callback_generator(idx))
 
     def upload_finished_callback_generator(self, idx):
         def callback(result):
@@ -212,21 +245,12 @@ class Uploader(object):
     def progress(self):
         return int(sum(self.result_set) * 100 / len(self.result_set))
 
-class UploadToDocument(Uploader):
-    def __init__(self, client, pool, document_id, sheet_count, directory):
-        self.document_id = document_id
-        self.job_id = None
-        super(UploadToDocument, self).__init__(client, pool, sheet_count, directory)
-        self.label = 'Document %s: %s' % (self.document_id, self.directory)
+class UploadToBatch(Uploader):
+    def __init__(self, client, pool, batch_id, directory):
+        self.batch_id = batch_id
+        super(UploadToBatch, self).__init__(client, pool, directory)
+        self.label = 'Batch %s: %s' % (self.batch_id, self.directory)
 
-class UploadToJob(Uploader):
-    def __init__(self, client, pool, job_id, sheet_count, directory):
-        self.job_id = job_id
-        super(UploadToJob, self).__init__(client, pool, sheet_count, directory)
-        self.label = 'Job %s: %s' % (self.job_id, self.directory)
-
-def upload_iset(api_token, job_id, files):
+def upload_file(api_token, batch_id, f):
     client = Client(api_token)
-    iset = client.create_instance_sets(job_id, {})
-    for i, fname in enumerate(files):
-        client.create_instance_set_instances(iset['id'], {'image_file': open(fname), 'page_number': str(i)})
+    client.create_batch_files(batch_id, {'uploaded_file': open(f)})
